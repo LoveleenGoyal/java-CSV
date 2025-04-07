f5a33209ce91da5befe4e9f28f4a2b4f8ceb1d77
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-04-07 14:13:33 +0530

    [Loveleen] ADD: Write data to csv file

--- src/main/java/com/bridgelabz/basicProblems/WriteCSVFile.java
+++ src/main/java/com/bridgelabz/basicProblems/WriteCSVFile.java
@@ -1,7 +1,25 @@
 package com.bridgelabz.basicProblems;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WriteCSVFile {
     public static void main(String[] args) {
+        String employeeFile = "employeeDetails.csv";
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(employeeFile));
 
+            writer.write("ID,Name,Department,Salary\n");
+            writer.write("104,Alice Williams,Finance,62000\n");
+            writer.write("101,Loveleen,HR,60000\n");
+            writer.write("107,Yagyata,Research,50000\n");
+            writer.write("102,Love,Finance,45000");
+            writer.close();
+
+            System.out.println("CSV file written successfully");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
