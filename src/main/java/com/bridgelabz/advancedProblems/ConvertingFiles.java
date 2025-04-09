7ca5040db3c5f22972b99141339d2ccf4f07db7d
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-04-09 14:41:44 +0530

    [Loveleen] ADD: Converting json to csv and vice versa

--- src/main/java/com/bridgelabz/advancedProblems/ConvertingFiles.java
+++ src/main/java/com/bridgelabz/advancedProblems/ConvertingFiles.java
@@ -2,7 +2,9 @@ package com.bridgelabz.advancedProblems;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvException;
 
 import java.io.*;
 import java.util.*;
@@ -26,11 +28,15 @@ public class ConvertingFiles {
     public static void main(String[] args) throws IOException {
         String jsonFile = "files/students.json";
         String csvFile = "files/csvStudents.csv";
+        String jsonFromCsv = "files/studentsFromCsv.json";
 
+        // Step1: JSON -> CSV
         ObjectMapper objectMapper = new ObjectMapper();
 
+        // Reads json file and maps to list
         List<Students> students = objectMapper.readValue(new File(jsonFile), new TypeReference<List<Students>>() {});
 
+        // Write list to Csv
         try (CSVWriter writer  = new CSVWriter(new FileWriter(csvFile))){
             writer.writeNext(new String[]{"ID", "Name", "Age", "Marks"});
             for (Students s : students) {
@@ -43,6 +49,28 @@ public class ConvertingFiles {
             }
         }
         System.out.println("Converted Json to Csv");
+
+
+        // Step2: CSV -> JSON
+        List<Students> studentsCsv = new ArrayList<>();
+        try (CSVReader reader = new CSVReader(new FileReader(csvFile))) {
+            reader.readNext();
+            String[] line;
+            while ((line = reader.readNext()) != null) {
+                int id = Integer.parseInt(line[0]);
+                String name = line[1];
+                int age = Integer.parseInt(line[2]);
+                int marks = Integer.parseInt(line[3]);
+
+                Students s = new Students(id,name,age,marks);
+                studentsCsv.add(s);
+            }
+        } catch (CsvException | IOException e) {
+            e.printStackTrace();
+        }
+
+        objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File(jsonFromCsv), studentsCsv);
+        System.out.println("Converted Csv to Json");
 
     }
 }
