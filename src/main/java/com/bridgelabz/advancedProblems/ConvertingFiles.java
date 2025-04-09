2422ad3b4c831532757eea1b31b3e353f2670cf9
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-04-09 14:05:10 +0530

    [Loveleen] ADD: Added jackson dependency

--- src/main/java/com/bridgelabz/advancedProblems/ConvertingFiles.java
+++ src/main/java/com/bridgelabz/advancedProblems/ConvertingFiles.java
@@ -1,4 +1,10 @@
 package com.bridgelabz.advancedProblems;
 
 public class ConvertingFiles {
+    String jsonFile = "students.json";
+    String csvFile = "csvStudents.csv";
+
+    public static void main(String[] args) {
+
+    }
 }
