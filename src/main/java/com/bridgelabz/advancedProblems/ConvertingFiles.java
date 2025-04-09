c9a3ca68d255d41b3ab41b4c4bd9ce94d5ede8a8
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-04-09 14:29:26 +0530

    [Loveleen] ADD: Converting json to csv and vice versa

--- src/main/java/com/bridgelabz/advancedProblems/ConvertingFiles.java
+++ src/main/java/com/bridgelabz/advancedProblems/ConvertingFiles.java
@@ -1,10 +1,48 @@
 package com.bridgelabz.advancedProblems;
 
-public class ConvertingFiles {
-    String jsonFile = "students.json";
-    String csvFile = "csvStudents.csv";
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.opencsv.CSVWriter;
 
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.*;
+
+class Students {
+    public int id;
+    public String name;
+    public int age;
+    public int marks;
+
+    public Students() {}
+
+    public Students(int id, String name, int age, int marks) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
+    }
+}
+public class ConvertingFiles {
+    public static void main(String[] args) throws IOException {
+        String jsonFile = "files/students.json";
+        String csvFile = "files/csvStudents.csv";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        List<Students> students = objectMapper.readValue(new File(jsonFile), new TypeReference<List<Students>>() {});
+
+        try (CSVWriter writer  = new CSVWriter(new FileWriter(csvFile))){
+            writer.writeNext(new String[]{"ID", "Name", "Age", "Marks"});
+            for (Students s : students) {
+                writer.writeNext(new String[]{
+                        String.valueOf(s.id),
+                        s.name,
+                        String.valueOf(s.age),
+                        String.valueOf(s.marks)
+                });
+            }
+        }
+        System.out.println("Converted Json to Csv");
 
     }
 }
